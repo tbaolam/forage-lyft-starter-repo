2f3c5f714942fad4fabae114c5448f722002395a
Bao Lam <thienbao0712@gmail.com>
2024-02-01 20:46:38 -0800

    Update carFactory.py

--- carFactory.py
+++ carFactory.py
@@ -10,26 +10,31 @@ from willoughbyEngine import WilloughbyEngine
 from sternmanEngine import SternmanEngine
 
 class CarFactory:
+    @staticmethod
     def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
         engine = CapuletEngine(last_service_mileage, current_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
         return Car(engine, battery)
     
+    @staticmethod
     def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
         engine = WilloughbyEngine(last_service_mileage, current_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
         return Car(engine, battery)
     
+    @staticmethod
     def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(last_service_date, current_date)
         return Car(engine, battery)
     
+    @staticmethod
     def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
         engine = WilloughbyEngine(last_service_mileage, current_mileage)
         battery = NubbinBattery(last_service_date, current_date)
         return Car(engine, battery)
     
+    @staticmethod
     def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
         engine = CapuletEngine(last_service_mileage, current_mileage)
         battery = NubbinBattery(last_service_date, current_date)
